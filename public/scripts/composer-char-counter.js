e1a8117249e16eb8c0edfad0b831aed283c51f35
tronross <john_ross@analogforever.ca>
2022-11-13 16:36:50 -0500

    Add logic to change counter color; add jQuery to print to counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 $(document).ready(function(event) {
-  $('#tweet-text').on('input', function(event){
+  $('#tweet-text').on('input', function(event) {
     const tweetText = $( this ).val();
     const count = 140 - tweetText.length;
     let counterNode = $( this ).parent().children('#submit-counter').children('.counter');
-    
+
     if (count < 0) {
       $( counterNode ).css({ color: 'red' });
     } else {
       $( counterNode ).css({ color: 'black' });
     }
     counterNode.text(count);
-  })
+  });
 });
